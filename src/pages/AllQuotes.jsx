015d712617ba7abd2f535e06c9967d48c3bc7129
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-11-30 13:35:08 +0100

    Adding Dummy Data & More Content

--- src/pages/AllQuotes.jsx
+++ src/pages/AllQuotes.jsx
@@ -1,5 +1,20 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Raul H",
+    text: "I'm learning a lot of React!",
+  },
+  {
+    id: "q2",
+    author: "John",
+    text: "Writting books is hard...",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All quotes here</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
